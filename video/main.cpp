c061e662a574c72af2e3b809cf1d55f32ce9de34
Sean Silva <silvas@purdue.edu>
2013-10-05 11:19:21 -0400

    Save some progress.
    This lets us show a frame from the video. Next step is to put the frame
    into a texture and send it to the GPU.

--- video/main.cpp
+++ video/main.cpp
@@ -1,9 +1,15 @@
 #include "openglwindow.h"
-#include <QGuiApplication>
+#include <QApplication>
 #include <QKeyEvent>
 #include <QOpenGLShaderProgram>
 #include <QScreen>
 #include <Qt>
+#include <QSize>
+#include <QPixmap>
+#include <QImage>
+#include <QLabel>
+#include <memory>
+#include <vector>
 
 const char VertexShaderSource[] = R"(
 attribute highp vec4 posAttr;
@@ -79,20 +85,20 @@ public:
     Program->bind();
 
     QMatrix4x4 M;
-    //M.ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-    //M.perspective(60, static_cast<qreal>(width()) / height(), 0.1, 100.0);
+    // M.ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    // M.perspective(60, static_cast<qreal>(width()) / height(), 0.1, 100.0);
     M.translate(0, UpDown, LeftRight);
 
-    //M.translate(0, 0, -2);
-    //M.rotate(100.0f * FrameNum / screen()->refreshRate(), 0, 1, 0);
+    // M.translate(0, 0, -2);
+    // M.rotate(100.0f * FrameNum / screen()->refreshRate(), 0, 1, 0);
 
     Program->setUniformValue(MatrixUniform, M);
 
-    Vertex Vertices[] = {                         //
-      { { -1.0f, -1.0f }, { 1.0f, 0.0f, 0.0f } }, //
-      { { -1.0f, 1.0f }, { 0.0f, 1.0f, 0.0f } },  //
-      { { 1.0f, -1.0f }, { 0.0f, 0.0f, 1.0f } },  //
-      { { 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f } },   //
+    Vertex Vertices[] = {                     //
+        {{-1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}}, //
+        {{-1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},  //
+        {{1.0f, -1.0f}, {0.0f, 0.0f, 1.0f}},  //
+        {{1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}},   //
     };
 
     GLuint VBOID;
@@ -132,8 +138,79 @@ private:
   int FrameNum = 0;
 };
 
+static const uchar *skipToAfterNewline(const uchar *P) {
+  while (*P != '\n')
+    ++P;
+  return P + 1;
+}
+
+class YUV4MPEG2 {
+public:
+  YUV4MPEG2(const uchar *RawContents_, size_t RawSize_)
+      : RawContents{RawContents_}, RawSize{RawSize_} {
+    // FIXME: Actually parse the header. For now, assume:
+    // - 352x288 4:2:0 YCbCr non-interleaved, one byte per sample.
+    const uchar *FrameHeader = skipToAfterNewline(RawContents);
+    while (FrameHeader < RawContents + RawSize) {
+      const uchar *Y = skipToAfterNewline(FrameHeader);
+      const uchar *Cb = skipLumaPlane(Y);
+      const uchar *Cr = skipChromaPlane(Cb);
+      Frames.push_back(Frame{Y, Cb, Cr});
+      FrameHeader = skipChromaPlane(Cr);
+    }
+  }
+  const uchar *skipLumaPlane(const uchar *P) { return P + Width * Height; }
+  const uchar *skipChromaPlane(const uchar *P) {
+    return P + (Width / 2) * (Height / 2);
+  }
+  struct Frame {
+    const uchar *Y;
+    const uchar *Cb;
+    const uchar *Cr;
+  };
+  const uchar *RawContents;
+  size_t RawSize;
+  std::vector<Frame> Frames;
+  int Width = 352;
+  int Height = 288;
+};
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args &&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+std::unique_ptr<QImage> createImageFromMonochromeBytes(const uchar *Bytes,
+                                                       int W, int H) {
+  auto Ret = make_unique<QImage>(QSize{W, H}, QImage::Format_RGB888);
+  uchar *RGBBuf = Ret->bits();
+  for (int i = 0, e = W * H; i != e; ++i) {
+    RGBBuf[3 * i] = Bytes[i];
+    RGBBuf[3 * i + 1] = Bytes[i];
+    RGBBuf[3 * i + 2] = Bytes[i];
+  }
+  return Ret;
+}
+
 int main(int argc, char *argv[]) {
-  QGuiApplication a(argc, argv);
+  QApplication A(argc, argv);
+
+  QFile F("/home/sean/tmp/foreman_cif.y4m");
+  if (!F.open(QIODevice::ReadOnly)) {
+    qDebug() << "Unable to open file";
+    return 1;
+  }
+  const uchar *RawFile = F.map(0, F.size());
+  if (!RawFile) {
+    qDebug() << "Unable to map file";
+    return 1;
+  }
+  YUV4MPEG2 Y4M{RawFile, (size_t)F.size()};
+  const uchar *FirstFrameLuma = skipToAfterNewline(skipToAfterNewline(RawFile));
+  auto Image = createImageFromMonochromeBytes(FirstFrameLuma, 352, 288);
+  QLabel L;
+  L.setPixmap(QPixmap::fromImage(*Image));
+  L.show();
 
   QSurfaceFormat Format;
   Format.setSamples(4);
@@ -143,5 +220,5 @@ int main(int argc, char *argv[]) {
   W.show();
   W.setAnimating(true);
 
-  return a.exec();
+  return A.exec();
 }
