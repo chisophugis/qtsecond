aeb7a43efc6e31de66c5bc27df55632f5047cc2f
Sean Silva <silvas@purdue.edu>
2013-10-13 20:24:06 -0400

    This is technically needed for non-power-of-two textures.

--- video/main.cpp
+++ video/main.cpp
@@ -146,6 +146,8 @@ public:
     GLuint Ret;
     glGenTextures(1, &Ret);
     glBindTexture(GL_TEXTURE_2D, Ret);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     return Ret;
